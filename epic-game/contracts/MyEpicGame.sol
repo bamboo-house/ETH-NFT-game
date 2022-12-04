02843527aceeb1a4d62536b4a534f3ac53c76f13
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2022-12-04 19:54:22 +0900

    ボスに攻撃する関数を追加

--- epic-game/contracts/MyEpicGame.sol
+++ epic-game/contracts/MyEpicGame.sol
@@ -132,4 +132,35 @@ contract MyEpicGame is ERC721 {
     );
     return output;
   }
+
+  function attackBoss() public {
+    // 1. プレイヤーのNFTの状態を取得します
+    uint256 nftTokenIdOfPlayer = nftHolders[msg.sender];
+    CharacterAttributes storage player = nftHolderAttributes[nftTokenIdOfPlayer];
+    console.log("\nPlayer w/ character %s about to attack. Has %s HP and %s AD", player.name, player.hp, player.attackDamage);
+    console.log("Boss %s has %s HP and %s AD", bigBoss.name, bigBoss.hp, bigBoss.attackDamage);
+
+    // 2. プレイヤーのHPが0以上であることを確認する
+    require (player.hp > 0, "Error: character must have HP to attack boss.");
+
+    // 3. ボスのHPが0以上であることを確認する。
+    require (bigBoss.hp > 0, "Error: boss must have HP to attack characters.");
+
+    // 4. プレイヤーがボスを攻撃できるようにする
+    if (bigBoss.hp < player.attackDamage) {
+      bigBoss.hp = 0;
+    } else {
+      bigBoss.hp = bigBoss.hp - player.attackDamage;
+    }
+
+    // 5. ボスがプレイヤーを攻撃できるようにする
+    if (player.hp < bigBoss.attackDamage) {
+      player.hp = 0;
+    } else {
+      player.hp = player.hp - bigBoss.attackDamage;
+    }
+
+    console.log("Player attacked boss. New boss hp: %s", bigBoss.hp);
+    console.log("Boss attacked player. New player hp: %s\n", player.hp);
+  }
 }
