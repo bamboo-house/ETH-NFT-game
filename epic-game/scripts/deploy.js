37042697347704acfe495aed027174afcec61a1d
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2022-12-06 18:28:06 +0900

    テストネットでの動作確認

--- epic-game/scripts/deploy.js
+++ epic-game/scripts/deploy.js
@@ -14,24 +14,21 @@ const main = async () => {
     10000, // Bossのhp
     50 // Bossの攻撃力
   );
-  await gameContract.deployed();
-  console.log("Contract deployed to:", gameContract.address);
-
+  const nftGame =  await gameContract.deployed();
+  console.log("Contract deployed to:", nftGame.address);
   let txn;
-
-  txn = await gameContract.mintCharacterNFT(0);
-  await txn.wait();
-  console.log("Minted NFT #1");
-
-  txn = await gameContract.mintCharacterNFT(1);
-  await txn.wait();
-  console.log("Minted NFT #2");
 
   txn = await gameContract.mintCharacterNFT(2);
   await txn.wait();
-  console.log("Minted NFT #3");
 
-  console.log("Done deploying and minting!");
+  txn = await gameContract.attackBoss();
+  await txn.wait();
+  console.log("First attack.");
+  txn = await gameContract.attackBoss();
+  await txn.wait();
+  console.log("Second attack.");
+
+  console.log("Done!");
 };
 
 const runMain = async () => {
