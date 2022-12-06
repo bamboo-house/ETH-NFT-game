c5044b518ff9d20ae1da8a652dffbe4bde36c30a
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2022-12-06 20:05:49 +0900

    update

---
+++ nft-game-starter-project/src/App.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from 'react';
+import twitterLogo from './assets/twitter-logo.svg';
+import './App.css';
+
+// Constants
+const TWITTER_HANDLE = 'あなたのTwitterハンドル';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const App = () => {
+  const [currentAccount, setCurrentAccount] = useState(null);
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        console.log("Make sure you have MetaMask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+        const accounts = await ethereum.request({ method: "eth_accounts" });
+        if (accounts.length !== 0) {
+          const account = accounts[0];
+          console.log("Found an authorized account:", account);
+          setCurrentAccount(account);
+        } else {
+          console.log("No authorized account found");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const connectWalletAction = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+        <p className="header gradient-text">⚡️ METAVERSE GAME ⚡️</p>
+          <p className="sub-text">プレイヤーと協力してボスを倒そう✨</p>
+          <div className="connect-wallet-container">
+            <img
+              src="https://i.imgur.com/TXBQ4cC.png"
+              alt="LUFFY"
+            />
+          </div>
+          <button
+            className="cta-button connect-wallet-button"
+            onClick={connectWalletAction}
+          >
+            Connect Wallet To Get Started
+          </button>
+        </div>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built with @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
