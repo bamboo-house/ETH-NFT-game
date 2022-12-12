10fddeceba0bb53a81d359c1e97026458446abfe
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2022-12-12 22:03:30 +0900

    NFTキャラレンダリング

--- nft-game-starter-project/src/Components/Arena/index.js
+++ nft-game-starter-project/src/Components/Arena/index.js
@@ -63,7 +63,29 @@ const Arena = ({ characterNFT }) => {
         </div>
       )}
       {/* NFT キャラクター */}
-      <p>NFT キャラクターを表示します。</p>
+      {characterNFT && (
+        <div className="players-container">
+          <div className="player-container">
+            <h2>Your Character</h2>
+            <div className="player">
+              <div className="image-content">
+                <h2>{characterNFT.name}</h2>
+                <img
+                  src={characterNFT.imageURI}
+                  alt={`Character ${characterNFT.name}`}
+                />
+                <div className="health-bar">
+                  <progress value={characterNFT.hp} max={characterNFT.maxHp} />
+                  <p>{`${characterNFT.hp} / ${characterNFT.maxHp} HP`}</p>
+                </div>
+              </div>
+              <div className="stats">
+                <h4>{`⚔️ Attack Damage: ${characterNFT.attackDamage}`}</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
